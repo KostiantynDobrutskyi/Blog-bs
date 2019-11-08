cbd469903fa3cd04f57af918564b9bc88412daba
KostiantynDobrutskyi <dobruzkij@gmail.com>
2019-11-08 11:31:39 +0200

    bug fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,52 @@
-var gulp = require("gulp"),
+const {src, dest, watch, parallel, series} = require('gulp'),
     sync = require("browser-sync").create(),
     del = require("del"),
     plugins = require("gulp-load-plugins")({
         scope: ["devDependencies"]
     });
 
-var IS_DEV = true;
-gulp.task("html", function () {
-    return gulp.src("src/views/*.html")
+
+function html() {
+    return src("src/views/*.html")
         .pipe(plugins.htmlExtend())
-        .pipe(gulp.dest("dist"))
+        .pipe(dest("dist"))
         .pipe(sync.stream())
-});
+}
 
-
-gulp.task("images", function () {
-    return gulp.src("src/images/**/*.*")
-        .pipe(gulp.dest("dist/images"))
-});
-
-
-
-gulp.task("styles:app", function () {
-    return gulp.src("src/styles/app.less")
+function stylesApp() {
+    return src("src/styles/app.less")
         .pipe(plugins.plumber())
         .pipe(plugins.less())
         .pipe(plugins.autoprefixer())
-        .pipe(plugins.cleanCss())
         .pipe(plugins.rename({suffix: ".min"}))
-        .pipe(gulp.dest("dist/css"))
+        .pipe(plugins.cssnano())
+        .pipe(dest("dist/css"))
         .pipe(sync.stream())
-});
+}
 
-gulp.task("clean", function (cb) {
+function images() {
+    return src(
+        ["src/images/**/*.*"]
+    )
+        .pipe(dest("dist/images"))
+}
+
+function clean(cb) {
     del.sync("dist");
     cb();
-});
+}
 
-gulp.task("build", ["clean"], function () {
-    gulp.start(["html", "styles:app", "images"]);
-});
-
-gulp.task("watch", ["build"], function () {
+function watcher() {
     sync.init({
         server: "dist"
     });
-    gulp.watch("src/styles/**/*.less", ["styles:app"]);
+    watch("src/styles/**/*.less", parallel(stylesApp));
+    watch("src/views/**/*.html", parallel(html));
 
-    gulp.watch("src/views/**/*.html", ["html"]);
-    // gulp.watch("dist/*.html").on("change", sync.reload());
+}
 
-});
-
-gulp.task("default", ["watch"]);
+exports.default = series(clean, parallel(
+    html,
+    stylesApp,
+    images,
+), watcher);
